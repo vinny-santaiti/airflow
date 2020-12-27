567850071ff361508a73bbdac516a2548404f4ee
Vincent Santaiti <vsantaiti@gmail.com>
2020-12-27 15:34:27 -0500

    Create airflow2.py

---
+++ airflow2.py
@@ -0,0 +1,15 @@
+"""
+The following instructions are for Mac
+
+curl -sSL https://install.astronomer.io | sudo bash -s -- v0.22.0
+
+mkdir airflow-2
+
+cd airflow-2
+
+astro dev init|start|stop|kill
+
+Change Dockerfile:
+
+FROM astronomerinc/ap-airflow:1.10.12-alpine3.10
+"""
