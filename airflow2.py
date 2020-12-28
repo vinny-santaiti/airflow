486d50e3ff8ba5e0013e779547a648e884182aef
Vincent Santaiti <vsantaiti@gmail.com>
2020-12-28 18:12:21 -0500

    Update airflow2.py
    reorder step

--- airflow2.py
+++ airflow2.py
@@ -8,10 +8,12 @@ mkdir airflow-2
 
 cd airflow-2
 
-astro dev init|start|stop|kill
+astro dev init
 
 Change Dockerfile for airflow 2
 https://hub.docker.com/r/astronomerio/ap-airflow/tags?page=1&ordering=last_updated&name=2.0.0
 
 FROM astronomerio/ap-airflow:2.0.0-buster-onbuild-22237
+
+astro dev start|stop|kill
 """
